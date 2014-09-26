92f425a7644f635e8db375623e16220916e63053
kwang2014 <358556758@qq.com>
2014-09-26 10:08:55 +0800

    new project of MVC
    test

---
+++ MVCDemo/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from Store.Index()";
+            return View();
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var albums = new List<Album>();
+            for (int i = 0; i < 10; i++)
+            {
+                albums.Add(new Album { Title = "Product: " + i });
+            }
+
+            return View(albums);
+        }
+
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+    }
+}
